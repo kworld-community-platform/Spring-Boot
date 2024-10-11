9ae4fe39914f1e5d6fdac1d35143e5b1aa610e8c
hyunjin <atomhyunjin@naver.com>
2024-10-12 02:31:28 +0900

    Initial commit

---
+++ src/main/java/com/hyunjin/kworld/KworldApplication.java
@@ -0,0 +1,13 @@
+package com.hyunjin.kworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KworldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hyunjin/kworld/KworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyunjin.kworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
