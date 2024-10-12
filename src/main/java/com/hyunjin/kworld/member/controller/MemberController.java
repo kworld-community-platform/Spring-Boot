b2fd607e4f56e02967321c698f09e086cb9745de
hyunjin <atomhyunjin@naver.com>
2024-10-12 16:57:28 +0900

    chore: update gitignore

--- src/main/java/com/hyunjin/kworld/member/controller/MemberController.java
+++ src/main/java/com/hyunjin/kworld/member/controller/MemberController.java
@@ -1,4 +1,12 @@
 package com.hyunjin.kworld.member.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
 public class MemberController {
+//    @PostMapping
+//    public String signup
 }
