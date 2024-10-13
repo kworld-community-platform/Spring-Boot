675390fe09a9ea4ebda14bc35530be2d88d20191
hyunjin9603 <hyunjin9603@gmail.com>
2024-10-14 01:43:16 +0900

    test: MemberRepository-emailExisting

--- src/test/java/com/hyunjin/kworld/member/repository/MemberRepositoryTest.java
+++ src/test/java/com/hyunjin/kworld/member/repository/MemberRepositoryTest.java
@@ -1,4 +1,35 @@
 package com.hyunjin.kworld.member.repository;
 
+import com.hyunjin.kworld.member.entity.Member;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import java.util.Optional;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
 public class MemberRepositoryTest {
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Test
+    @DisplayName("DB에 존재하는 계정")
+    public void existEmail() {
+        Member member = new Member("test@kw.ac.kr", "12kwukwu", "조광운");
+        memberRepository.save(member);
+
+        Optional<Member> findByEmail = memberRepository.findByEmail("test@kw.ac.kr");
+
+        assertThat(findByEmail).isPresent();
+        assertThat(findByEmail.get().getEmail()).isEqualTo("test@kw.ac.kr");
+    }
+
+    @Test
+    @DisplayName("DB에 존재하지 않는 계정")
+    public void nonExistEmail(){
+        Optional<Member>findByEmail = memberRepository.findByEmail("kwu@kw.ac.kr");
+        assertThat(findByEmail).isEmpty();
+    }
 }
